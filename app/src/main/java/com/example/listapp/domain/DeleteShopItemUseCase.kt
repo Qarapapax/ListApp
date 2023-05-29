701edefe003a3543a18b7419448e4510fa9f0b5b
Qarapapax <aldunsar@yahoo.com>
2023-05-29 15:32:32 +0300

    add repository to domain layer

---
+++ app/src/main/java/com/example/listapp/domain/DeleteShopItemUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.listapp.domain
+
+class DeleteShopItemUseCase(private val shopListRepository: ShopListRepository) {
+    fun deleteShopItem(shopItem: ShopItem) {
+        shopListRepository.deleteShopItem(shopItem)
+    }
+}
--- app/src/main/java/com/example/listapp/domain/DeletehopItemUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.example.listapp.domain
-
-class DeletehopItemUseCase {
-    fun deleteShopItem(shopItem: ShopItem) {
-
-    }
-}
